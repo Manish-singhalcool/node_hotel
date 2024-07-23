9fbe0f3a7da84d1933c87d2c33cca082e5cd09b7
manish.singhal <manish.singhal@rapidsoft.co.in>
2024-07-23 22:15:52 +0530

    env proccess apply

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 //const { default: mongoose } = require('mongoose');
 const mongoose= require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/hotels'
-
+require('dotenv').config();
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL,{
      
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 const db = require('./db');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+require('dotenv').config();
 
 const personLink = require('./routes/personRoutes');
 const menuLink = require('./routes/menuRoutes');
 
 
-    const port = 3000;
+    const port = process.env.PORT || 3000;
     app.get('/home',(req,resp)=>{
         resp.send(`hello Home <br><a href="/about">Click Me </a>` )
     })

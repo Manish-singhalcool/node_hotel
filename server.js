72a0f2ecaa9c6fd3092e62d00dfc6991ba84cabd
manish.singhal <manish.singhal@rapidsoft.co.in>
2024-07-25 14:49:40 +0530

    middleware apply

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const personLink = require('./routes/personRoutes');
 const menuLink = require('./routes/menuRoutes');
 
     const port = process.env.PORT || 3000;
+
+    const loadRequest= (req,res,next)=>{
+        console.log(`[${new Date().toLocaleString()}] request  made  to : ${req.originalUrl}`);
+        next();
+    }
+
+    
+
     app.get('/home',(req,resp)=>{
         resp.send(`hello Home <br><a href="/about">Click Me </a>` )
     })
@@ -16,7 +24,7 @@ const menuLink = require('./routes/menuRoutes');
         resp.send('hello About <br><a href="/home">Click Me </a>')
     })
     
-    app.use('/', personLink);
+    app.use('/',loadRequest, personLink);
     app.use('/', menuLink);
 
 app.listen(port, ()=>{
